e8123e7494667a5e07e62618b4adbe94de864dfd
Likhitha <likhitha.nanda@gmail.com>
2018-05-11 18:20:08 -0500

    message

--- NaiveBayes.py
+++ NaiveBayes.py
@@ -1,242 +1,219 @@
 import os
 import sys
 import re
-# def readData:
-#     file=open(data_in)
-#     read=csv.reader(file)
-    # arr=[]
-words=[]
-
-
+import math
+final_dataTest=[]
+dictionaryArrays=[]
 def getFiles(path):
-    d=dict()
-    for filename in os.scandir(path):
-        if filename.is_file():
-            file=open(filename)
-            i=0
-            lines=0
-            for row in file:
-                if row.__contains__("Lines: "):
-                    # if len(row.split("Lines: ")) == 2:
-                    lines=int(row.split("Lines:")[1])
-                    continue
-                if (lines !=0 and i<=lines):
-                    i+=1
-                    if row != "\n":
-                        process(row,d)
-                elif(lines !=0):
-                    break
-    return d
+	array1=[]
+	Sum=0
+	value=0
+	d={}
+	for filename in os.scandir(path):
+		if filename.is_file():
+			count=0
+			f=open(filename,'r')
+			for row in f:
+				beg=0
+				end=0
+				count=count+1
+				if row.find("Lines:")>-1:
+					beg=count
+					#print("beg:",beg)
+					array1=int(row.split("Lines:")[1])
+					end=beg+array1
+					#print("end:",end)
+					CleanData(f,beg,end,d)
+	#od=collections.OrderedDict(sorted(d.items()))
+	for key in d:
+		value=d.get(key,1)
+		Sum= Sum+int(value)
+	#print(value,"Value")
+	#print("Sum",Sum)
+	return d,Sum
+			
+def CleanData(f,beg,end,d):
+	for index, line in enumerate(f):
+		if 0 <= index <= end:
+			line = re.sub(r"[^a-zA-Z0-9]+"," ",line.lower())
+			if(line != " "):
+				nostop(line,d)
+	return d
+	
+def nostop(line,d):
+	stopwords = ["","a","about","above","after","again","against","all","am","an","and", "any","are","aren't","as","at","be","because","been","before","being",
+        "below","between","both","but","by","can't","cannot","could","couldn't","did","didn't","do","does","doesn't","doing","don't","down",
+        "during","each","few","for","from","further","had","hadn't","has","hasn't","have","haven't","having","he","he'd","he'll","he's","her","here",
+        "here's","hers","herself","him","himself","his","how","how's","i","i'd","i'll","i'm","i've","if","in","into","is","isn't","it","it's", "its",
+        "itself","let's","me","more","most","mustn't","my","myself","no","nor","not", "of","off","on","once","only","or","other","ought","our","ours",
+        "ourselves","out","over","own","same","shan't","she","she'd","she'll","she's","should","shouldn't","so","shall","some","such","than","that",
+        "that's","the","their","theirs","them","themselves","then","there","there's","these","they","they'd","they'll","they're","they've","this","those",
+        "through","to","too","under","until","up","very","was","wasn't","we","we'd","we'll","we're","we've","were","weren't","what","what's","when",
+        "when's","where","where's","which","while","who","who's","whose","whom","why","why's","with","won't","would","wouldn't","you","you'd","you'll",
+        "you're","you've","your","yours","yourself","yourselves"]
+	l={}
+	final_data=[]
+	token=[]
+	token=line.split(" ")
+	for p in token:
+		if p not in stopwords:
+			final_data.append(p)
+	#print("final_data",final_data)
+	
+	for y in final_data:
+		if y not in l:
+			l[y]=1
+		else:
+			l[y]+=1
+	for h in l:
+		if h not in d:
+			d[h]=l[h]
+		else:
+			d[h]+=l[h]
+	return(d)
 
-def process(row,d):
-    global words
-    ignore=[
-        "",
-        "a",
-        "about",
-        "above",
-        "after",
-        "again",
-        "against",
-        "all",
-        "am",
-        "an",
-        "and",
-        "any",
-        "are",
-        "aren't",
-        "as",
-        "at",
-        "be",
-        "because",
-        "been",
-        "before",
-        "being",
-        "below",
-        "between",
-        "both",
-        "but",
-        "by",
-        "can't",
-        "cannot",
-        "could",
-        "couldn't",
-        "did",
-        "didn't",
-        "do",
-        "does",
-        "doesn't",
-        "doing",
-        "don't",
-        "down",
-        "during",
-        "each",
-        "few",
-        "for",
-        "from",
-        "further",
-        "had",
-        "hadn't",
-        "has",
-        "hasn't",
-        "have",
-        "haven't",
-        "having",
-        "he",
-        "he'd",
-        "he'll",
-        "he's",
-        "her",
-        "here",
-        "here's",
-        "hers",
-        "herself",
-        "him",
-        "himself",
-        "his",
-        "how",
-        "how's",
-        "i",
-        "i'd",
-        "i'll",
-        "i'm",
-        "i've",
-        "if",
-        "in",
-        "into",
-        "is",
-        "isn't",
-        "it",
-        "it's",
-        "its",
-        "itself",
-        "let's",
-        "me",
-        "more",
-        "most",
-        "mustn't",
-        "my",
-        "myself",
-        "no",
-        "nor",
-        "not",
-        "of",
-        "off",
-        "on",
-        "once",
-        "only",
-        "or",
-        "other",
-        "ought",
-        "our",
-        "ours",
-        "ourselves",
-        "out",
-        "over",
-        "own",
-        "same",
-        "shan't",
-        "she",
-        "she'd",
-        "she'll",
-        "she's",
-        "should",
-        "shouldn't",
-        "so",
-        "some",
-        "such",
-        "than",
-        "that",
-        "that's",
-        "the",
-        "their",
-        "theirs",
-        "them",
-        "themselves",
-        "then",
-        "there",
-        "there's",
-        "these",
-        "they",
-        "they'd",
-        "they'll",
-        "they're",
-        "they've",
-        "this",
-        "those",
-        "through",
-        "to",
-        "too",
-        "under",
-        "until",
-        "up",
-        "very",
-        "was",
-        "wasn't",
-        "we",
-        "we'd",
-        "we'll",
-        "we're",
-        "we've",
-        "were",
-        "weren't",
-        "what",
-        "what's",
-        "when",
-        "when's",
-        "where",
-        "where's",
-        "which",
-        "while",
-        "who",
-        "who's",
-        "whom",
-        "why",
-        "why's",
-        "with",
-        "won't",
-        "would",
-        "wouldn't",
-        "you",
-        "you'd",
-        "you'll",
-        "you're",
-        "you've",
-        "your",
-        "yours",
-        "yourself",
-        "yourselves"
-        ]
-    # row = re.sub("\s+\n+[^a-zA-Z]*"," ",row)
+def getFiles_test(pth,dictionaryArrays,ind,Sum_Arr,prior_array):
+	#print("in test")
+	array1test=[]
+	correct=0
+	wrong=0
 
-    #row = re.sub("[^a-zA-Z]*"," ",row)
-    
-    row = re.sub(r"[^a-zA-Z0-9']+"," ",row)
-    row = row.split(" ")
-    for x in row:
-        if (x.lower() not in ignore):
-            words.append(x)
-    
-    for word in words:
-        if word not in d:
-            d[word]=1
-        else:
-            d[word]=d[word]+1
-    
-def sortRev(d):
-    return sorted(d.items(), key=lambda value: value[1], reverse=True)
+	for filename in os.scandir(pth):
+		#print("for class in test")
+		if filename.is_file():
+			#print("for one file")
+			FinalTestToken=[]
+			count=0
+			fTest=open(filename,'r')
+			for row in fTest:
+				begTest=0
+				endTest=0
+				count=count+1
+				if row.find("Lines:")>-1:
+					begTest=count
+					#print("beg:",beg)
+					array1test=int(row.split("Lines:")[1])
+					endTest=begTest+array1test
+					#print("end:",end)
+					CleanDataTest(fTest,begTest,endTest,FinalTestToken)
+		#print("for each file",FinalTestToken)
+		correct,wrong=conditional_probs(FinalTestToken,dictionaryArrays,ind,Sum_Arr,prior_array,correct,wrong)
+	#print ("correct",correct)
+	#print ("wrong",wrong)
+	print("accuracy of",pth,(correct/(correct+wrong))*100)
+	return correct,wrong
 
+def conditional_probs(FinalTestToken,dictionaryArrays,ind,Sum_Arr,prior_array,correct,wrong):
+	#print("for each file in one class")
+	likelihood=0.0
+	posterior_array=[]
+	for j in range(len(dictionaryArrays)):
+		#print("loop",j)
+		loglik=0
+		for word in FinalTestToken:
+			#print("word",word)
+			value=dictionaryArrays[j].get(word,0)
+			#print("value",value)
+			likelihood=(value+1)/(Sum_Arr[j]+len(dictionaryArrays[j]))
+			loglik+=math.log(likelihood)
+			#print("log likelihood",loglik)
+		posterior=loglik+math.log(prior_array[j])
+		#print("posterior",posterior)
+		posterior_array.append(posterior)
+	#print("array post",posterior_array)
+	pred_ind=posterior_array.index(max(posterior_array))
+	return accuracy(ind, pred_ind,correct,wrong)
+
+	
+def accuracy(ind, pred_ind,correct,wrong):
+	if (ind==pred_ind):
+		correct+=1
+	else:
+		wrong+=1
+	return correct,wrong
+
+def CleanDataTest(fTest,begTest,endTest,FinalTestToken):
+	final_dataTest=[]
+	for index, line in enumerate(fTest):
+		if 0 <= index <= endTest:
+			line = re.sub(r"[^a-zA-Z0-9]+"," ",line.lower())
+			if(line != " "):
+				nostopTest(line,FinalTestToken)
+			
+
+def nostopTest(line,FinalTestToken):
+	stopwords = ["","a","about","above","after","again","against","all","am","an","and", "any","are","aren't","as","at","be","because","been","before","being",
+        "below","between","both","but","by","can't","cannot","could","couldn't","did","didn't","do","does","doesn't","doing","don't","down",
+        "during","each","few","for","from","further","had","hadn't","has","hasn't","have","haven't","having","he","he'd","he'll","he's","her","here",
+        "here's","hers","herself","him","himself","his","how","how's","i","i'd","i'll","i'm","i've","if","in","into","is","isn't","it","it's", "its",
+        "itself","let's","me","more","most","mustn't","my","myself","no","nor","not", "of","off","on","once","only","or","other","ought","our","ours",
+        "ourselves","out","over","own","same","shan't","she","she'd","she'll","she's","should","shouldn't","so","shall","some","such","than","that",
+        "that's","the","their","theirs","them","themselves","then","there","there's","these","they","they'd","they'll","they're","they've","this","those",
+        "through","to","too","under","until","up","very","was","wasn't","we","we'd","we'll","we're","we've","were","weren't","what","what's","when",
+        "when's","where","where's","which","while","who","who's","whose","whom","why","why's","with","won't","would","wouldn't","you","you'd","you'll",
+        "you're","you've","your","yours","yourself","yourselves"]
+	
+	finalDataTest=[]
+	token_dataTest=[]
+	token=[]
+	token=line.split(" ")
+	for p in token:
+		if p not in stopwords:
+			token_dataTest.append(p)
+	#print("Test Tokens each line",token_dataTest)
+	for e in token_dataTest:
+		FinalTestToken.append(e)
+		
+def prior(Sum_Arr):
+	total=0
+	priors=[]
+	for i in range(len(Sum_Arr)):
+			total+=Sum_Arr[i]
+	#print("total",total)
+	for i in range(len(Sum_Arr)):
+		prior=Sum_Arr[i]/total
+		priors.append(prior)
+	#print("priors",priors)
+	return priors	
+	
 def main(args):
-    l=["20news-bydate-train\\alt.atheism","20news-bydate-train\\comp.graphics"]
-    data=[]
-    d=()
-    for i in range(len(l)):
-        d=getFiles(l[i])
-        d=sortRev(d)
-        
-        data.append(d)
-
-    #print(reversed(sortD))
-    print(data)
-
+	Sum_Arr=[]
+	#print("for training Data")
+	train=[f for f in os.listdir(args[1])]
+	#print(train)
+	dictionaryArrays=[]
+	for f in train:
+		#print("for each train")
+		#d1={}
+		pathTrain=args[1]+"\\"+f
+		#print("p",pathTrain)
+		#print("For each class")
+		d1,Sum=getFiles(pathTrain)
+		Sum_Arr.append(Sum)
+		#print("sum array",Sum_Arr)
+		dictionaryArrays.append(d1) #here calculate the length of dictinonaries
+		prior_array=prior(Sum_Arr)
+	#print("For Test Data")
+	test=[h for h in os.listdir(args[2])]
+	#print(test)
+	ind=0
+	c=0
+	w=0
+	for r in test:
+		tok1=[]
+		#print("for each test")
+		pathTest=args[2]+"\\"+r
+		#print("path Test",pathTest)
+		correct,wrong=getFiles_test(pathTest,dictionaryArrays,ind,Sum_Arr,prior_array)
+		c+=correct
+		w+=wrong
+		ind+=1
+	
+	print("total accuracy",(c/(c+w))*100)
+	#print("dictionary arrays",dictionaryArrays)
+	#print("for Test data")
+	#test1='temptest\\alt.atheism'
+	#print("for test1")
+	#tok1=getFiles_test(test1)
+	
 main(sys.argv)
